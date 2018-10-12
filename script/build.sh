bfa662838dd8213377ed54cddca5ffc8c3518728
Alex Kovrigin <a.kovrigin0@gmail.com>
2018-10-12 22:44:17 +0300

    try shell, start dev of doxygen

---
+++ script/build.sh
@@ -0,0 +1,5 @@
+qmake "QMAKE_CXXFLAGS += -std=c++0x" "QT += gui" "QT += core" "QT += widgets" "DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x040900"
+make debug
+
+qmake "QMAKE_CXXFLAGS += -std=c++0x" "QT += gui" "QT += core" "QT += widgets" "DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x040900"
+make release
