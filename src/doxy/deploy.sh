6e7f6a9fa6718ed5bc8b7cab75eb14cb93588b06
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-17 19:47:09 +0000

    Deploy waleko/PictureCrypt to github.com/waleko/PictureCrypt.git:gh-pages

--- src/doxy/deploy.sh
+++ src/doxy/deploy.sh
@@ -3,6 +3,10 @@
 # Doxygen running
 doxygen Doxyfile
 
+# Move pre-made index from doxy to root
+mv index.html ../../index.html
+
+# Generate pdf from tex
 cd ../../latex
 make
 mv refman.pdf ../refman.pdf
