9b3d2ace4280ecd5094aa14a327f23320582dacc
Alex Kovrigin <a.kovrigin0@gmail.com>
2018-12-17 16:07:26 +0300

    test_value test (deploy with windows) v6

--- scripts/deploy_windows.sh
+++ scripts/deploy_windows.sh
@@ -10,8 +10,9 @@ sudo apt-get install -y awscli
 #echo ":: set aws_region"
 #aws configure set default_region_name eu-north-1
 echo ":: download mxe"
+echo $TEST_VALUE
 aws s3 sync s3://mxe-qt/ mxe --region us-west-1
-export PATH=/home/travis/PictureCrypt/mxe/usr/bin:$PATH
+export PATH=$HOME/travis/PictureCrypt/mxe/usr/bin:$PATH
 cd src
 echo ":: qmake"
 ../mxe/usr/bin/i686-w64-mingw32.static-qmake-qt5 "QMAKE_CXXFLAGS += -std=c++0x" "QT += gui" "QT += core" "QT += widgets" "DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x040900"
