7a86c03d71733a7ef333bfb93e86db2087a749b0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-04 21:00:11 +0000

    Deploy waleko/PictureCrypt to github.com/waleko/PictureCrypt.git:gh-pages

--- app/scripts/install.sh
+++ app/scripts/install.sh
@@ -12,14 +12,10 @@ sudo apt-get install -y qt511-meta-minimal
 #sudo apt-get install -y qt5-dev-tools
 
   # Install g++
-sudo apt-get install -y g++-7
+sudo apt-get install -y g++-6
   # Install doxy + graphviz
 sudo apt-get install -y doxygen
 sudo apt-get install -y doxygen-doc
 sudo apt-get install -y doxygen-latex
 sudo apt-get install -y doxygen-gui
 sudo apt-get install -y graphviz
-
-  # Setup qt
-source /opt/qt511/bin/qt511-env.sh
-export QTHOME=/opt/qt511
