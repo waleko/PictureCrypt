e9c4d7862c7929620e345f99c893f04058188961
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-04 22:05:20 +0000

    Deploy waleko/PictureCrypt to github.com/waleko/PictureCrypt.git:gh-pages

--- app/scripts/build.sh
+++ app/scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-qmake "QMAKE_CXXFLAGS += -std=c++0x" "QT += gui" "QT += core" "QT += widgets" "QT += testlib" "DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x040900"
+qmake "QMAKE_CXXFLAGS += -std=c++0x" "QT += gui" "QT += core" "QT += widgets" "QT += testlib" "QT += xml" "DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x040900"
 make all
 
 cd src
@@ -9,6 +9,8 @@ sh ../scripts/clean.sh
 cd ../tests
 sh ../scripts/clean.sh
 
+cd ..
+sh scripts/clean.sh
 #cd ../tests
 #qmake "QMAKE_CXXFLAGS += -std=c++0x" "QT += gui" "QT += core" "QT += widgets" "QT += testlib" "DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x040900"
 #make
