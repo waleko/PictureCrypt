f4cd11df61cfd8e9d7c5d326c4390b02ce14dfc9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-25 19:21:21 +0000

    Deploy waleko/PictureCrypt to github.com/waleko/PictureCrypt.git:gh-pages

--- choco/tools/chocolateyInstall.ps1
+++ choco/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $name = 'PictureCrypt'
 $installerType = 'exe'
-$url  = 'https://storage.googleapis.com/waleko-personal.appspot.com/PictureCrypt-setup.exe'
+$url  = '.\PictureCrypt-setup.exe'
 $silentArgs = '/S'
 
 Install-ChocolateyPackage $name $installerType $silentArgs $url
